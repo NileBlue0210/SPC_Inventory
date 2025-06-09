8353d923fe7ad969f096ea17bd16254912add0df
DESKTOP-FVBU6NK\Sejin <sjb0710@gmail.com>
2025-06-09 10:19:09 +0900

    Feat: 인벤토리 UI 틀 구현, UIManager 작성
    UIManager를 싱글톤으로 구현, 매니저에 의해 관리되는 UI 스크립트 연결

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    public static UIManager Instance { get { return instance; } }
+
+    [SerializeField] private UIMainMenu mainMenu;
+    [SerializeField] private UIStatus status;
+    [SerializeField] private UIInventory inventory;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
